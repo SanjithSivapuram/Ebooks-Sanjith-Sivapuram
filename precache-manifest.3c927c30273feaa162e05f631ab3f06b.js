a44b02263e7a859766c0daa915eeac35b2d6906c
Sanjith Sivapuram <sanjithsivapuram@gmail.com>
2020-09-13 21:25:24 +0530

    Updates

---
+++ precache-manifest.3c927c30273feaa162e05f631ab3f06b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a13939f0713eb1f0295f625941a7c2",
+    "url": "/Ebooks-Sanjith-Sivapuram/index.html"
+  },
+  {
+    "revision": "887373654f37a6c1aa88",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/css/2.1d08211b.chunk.css"
+  },
+  {
+    "revision": "365d6780099924b63f3a",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/css/main.5cb5748a.chunk.css"
+  },
+  {
+    "revision": "887373654f37a6c1aa88",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/js/2.730ae887.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/js/2.730ae887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365d6780099924b63f3a",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/js/main.f70ac598.chunk.js"
+  },
+  {
+    "revision": "066d53c0bc486578f5ef",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/js/runtime-main.2bce5081.js"
+  },
+  {
+    "revision": "809949fcda59df7c8b321d9f8802d050",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/bookbg.809949fc.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ebooks-Sanjith-Sivapuram/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
